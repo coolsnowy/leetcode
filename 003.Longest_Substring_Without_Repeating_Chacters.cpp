747409570d41277e8a2cb0b8c7568209f9348981
coolsnowy <lc.shang.cn@gmail.com>
2018-06-10 13:35:16 +0800

    change the 003, add a new method

--- 003.Longest_Substring_Without_Repeating_Chacters.cpp
+++ 003.Longest_Substring_Without_Repeating_Chacters.cpp
@@ -69,6 +69,29 @@ public:
         }
         return maxLen;
     }
+    // see the 076th problem, got this method
+    int lengthOfLongestSubstring1(string s) {
+		vector<int> map(128,0);
+		// counter represent the num of the character which has appear over one time
+		int counter = 0, begin = 0, end = 0, d = 0;
+		while(end<s.size()){
+			// map > 0, means this character has seen at least once
+			if(map[s[end]] > 0)
+				counter++;
+			map[s[end]]++;
+			end++;
+			// now dissatisfy the requirement, we should change it until it is valid again, and then update max length
+			while(counter > 0) {
+				// the character appear only once should  == 1, over one time > 1
+				if(map[s[begin]] > 1)
+					counter--;
+				map[s[begin]]--;
+				begin++;
+			}
+			d = max(d, end-begin); //while valid, update d
+		}
+		return d;
+	}
 };
 int main() {
     Solution s;
